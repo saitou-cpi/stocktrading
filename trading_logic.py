c26dbcaec77f47a8bfabb0b5b896c2d4fec852fb
saitou-cpi <odeco_de_odeco@yahoo.co.jp>
2024-07-18 17:42:30 +0900

    vars.py修正

--- trading_logic.py
+++ trading_logic.py
@@ -5,12 +5,12 @@ def trading_logic(price, capital, holding_quantity, average_purchase_price):
     quantity = 0
 
     # ルール1: 取得した金額より{upper_limit}%上がったら売る
-    if holding_quantity > 0 and price >= average_purchase_price * {upper_limit}:
+    if holding_quantity > 0 and price >= average_purchase_price * upper_limit:
         action = 'sell'
         quantity = holding_quantity
 
     # ルール2: 取得した金額より{lower_limit}%下がったら売る
-    elif holding_quantity > 0 and price <= average_purchase_price * {lower_limit}:
+    elif holding_quantity > 0 and price <= average_purchase_price * lower_limit:
         action = 'sell'
         quantity = holding_quantity
 
--- vars.py
+++ vars.py
@@ -2,5 +2,5 @@ ticker_symbol = '4246.T' # ターゲット銘柄
 initial_capital = 50000 # 初期所持金
 base_url = "https://api.rakuten-sec.co.jp"
 api_key = "YOUR_API_KEY"
-upper_limit = 1.10
-lower_limit = 0.95
+upper_limit = 1.01
+lower_limit = 0.97
