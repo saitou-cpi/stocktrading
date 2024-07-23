ccc34dc3c8a10be712501a874d67b841492bd4b2
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-23 13:32:34 +0900

    Create vars.py

---
+++ ref/config/vars.py
@@ -0,0 +1,11 @@
+# config/vars.py
+
+# ターゲット銘柄（複数対応）4文字
+ticker_symbols = ['4246', '4820']
+
+# 初期所持金
+initial_capital = 100000
+
+# パラメータの組み合わせ float
+upper_limits = [1.01, 1.05, 1.10, 1.15, 1.20]
+lower_limits = [0.90, 0.95, 0.97, 0.99, 1.00]
