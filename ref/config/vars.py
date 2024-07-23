28c28254ef244413613e7bb207bdded01b6c367e
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-23 13:53:14 +0900

    Update vars.py

--- ref/config/vars.py
+++ ref/config/vars.py
@@ -1,6 +1,6 @@
 # config/vars.py
 
-# ターゲット銘柄（複数対応）4文字
+# ターゲット銘柄（複数対応）
 ticker_symbols = ['4246', '4820']
 
 # 初期所持金
@@ -9,3 +9,9 @@ initial_capital = 100000
 # パラメータの組み合わせ float
 upper_limits = [1.01, 1.05, 1.10, 1.15, 1.20]
 lower_limits = [0.90, 0.95, 0.97, 0.99, 1.00]
+
+# 移動平均の期間
+short_term_window = 5
+long_term_window = 10
+trend_short_term_window = 5
+trend_long_term_window = 10
