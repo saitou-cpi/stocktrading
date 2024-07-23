d26e2fd834535bd5a718091bfb9ffa302255f252
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-23 14:47:39 +0900

    Update vars.py

--- ref/config/vars.py
+++ ref/config/vars.py
@@ -1,4 +1,5 @@
 '''config/vars.py'''
+import numpy as np
 
 # ターゲット銘柄（複数対応）
 ticker_symbols = ['3350', '2112']
@@ -6,9 +7,14 @@ ticker_symbols = ['3350', '2112']
 # 初期所持金
 initial_capital = 100000
 
-# パラメータの組み合わせ float
-upper_limits = [1.01, 1.05, 1.10, 1.15, 1.20]
-lower_limits = [0.90, 0.95, 0.97, 0.99, 1.00]
+# パラメータの範囲指定
+upper_limit_range = (1.01, 1.20)
+lower_limit_range = (0.90, 0.99)
+steps = 0.01
+
+# パラメータの組み合わせ
+upper_limits = np.round(np.arange(upper_limit_range[0], upper_limit_range[1] + steps, steps), 2).tolist()
+lower_limits = np.round(np.arange(lower_limit_range[0], lower_limit_range[1] + steps, steps), 2).tolist()
 
 # 移動平均の期間
 short_term_window = 5
