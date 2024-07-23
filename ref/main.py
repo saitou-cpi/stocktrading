685d78ac7188dfa5876c6470e3d01ce39040be9e
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-23 13:48:09 +0900

    Update main.py

--- ref/main.py
+++ ref/main.py
@@ -1,14 +1,4 @@
-import subprocess
-import sys
 from optimal_parameter_finder import main
 
-def run_get_stock_month():
-    result = subprocess.run([sys.executable, 'get_stock_month.py'])
-    if result.returncode != 0:
-        print("Failed to run get_stock_month.py")
-        sys.exit(result.returncode)
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "get":
-        run_get_stock_month()
     main()
