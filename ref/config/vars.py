9f4a84b6869cc50da37897a64f195e75db415414
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-24 11:31:10 +0900

    Update vars.py

--- ref/config/vars.py
+++ ref/config/vars.py
@@ -12,12 +12,13 @@ upper_limit_range = (1.01, 1.20)
 lower_limit_range = (0.90, 0.99)
 steps = 0.01
 
-min_data_points = 30
-
 # パラメータの組み合わせ
 upper_limits = np.round(np.arange(upper_limit_range[0], upper_limit_range[1] + steps, steps), 2).tolist()
 lower_limits = np.round(np.arange(lower_limit_range[0], lower_limit_range[1] + steps, steps), 2).tolist()
 
-# 移動平均の期間
+# 算出期間 
+min_data_points = 30
+
+# 移動平均の期間 算出期間より少ない値
 short_term_window = 5
 long_term_window = 10
