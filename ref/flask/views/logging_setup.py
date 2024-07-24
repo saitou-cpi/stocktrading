88a3e42f2d51e214a545f13b6809fe3ece27febc
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-24 15:33:43 +0900

    Create logging_setup.py

---
+++ ref/flask/views/logging_setup.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+import logging
+
+def setup_logging(symbol):
+    log_dir = os.path.abspath("optimal_parameter_log")
+    os.makedirs(log_dir, exist_ok=True)
+    results_date_str = datetime.datetime.now().strftime('%Y%m%d%H%M')
+    log_filename = os.path.join(log_dir, f'{symbol.replace(".", "_")}_optimal_parameter_{results_date_str}.log')
+    logging.basicConfig(filename=log_filename, level=logging.INFO)
+    return log_dir
