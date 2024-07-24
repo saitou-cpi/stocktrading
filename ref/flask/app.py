e3bb0a9e88d9d677f1dd8ce10230baebb5dde2fb
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-24 16:50:39 +0900

    Update app.py

--- ref/flask/app.py
+++ ref/flask/app.py
@@ -11,13 +11,9 @@ def index():
 @app.route('/optimize', methods=['POST'])
 def optimize():
     ticker_symbol = request.form['ticker']
-    initial_capital = request.form.get('initial_capital', type=float)
-
+    initial_capital = request.form.get('initial_capital', type=int)
     if not ticker_symbol:
         return jsonify({"error": "Ticker symbol is required"}), 400
-
-    if not initial_capital or initial_capital < 1:
-        return jsonify({"error": "Valid initial capital is required"}), 400
 
     if not check_ticker_symbol(ticker_symbol):
         return jsonify({"error": "Ticker symbol not found in the database"}), 400
@@ -27,6 +23,10 @@ def optimize():
         return jsonify({"error": "売買不成立"}), 400
 
     return jsonify(result)
+
+@app.route('/help')
+def help():
+    return render_template('helppage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
