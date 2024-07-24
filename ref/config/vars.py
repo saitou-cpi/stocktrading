519f77e4cab2945f5cba477eadbaa1a159fd88af
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-24 11:15:06 +0900

    Update vars.py

--- ref/config/vars.py
+++ ref/config/vars.py
@@ -12,6 +12,8 @@ upper_limit_range = (1.01, 1.20)
 lower_limit_range = (0.90, 0.99)
 steps = 0.01
 
+MIN_DATA_POINTS = 30
+
 # パラメータの組み合わせ
 upper_limits = np.round(np.arange(upper_limit_range[0], upper_limit_range[1] + steps, steps), 2).tolist()
 lower_limits = np.round(np.arange(lower_limit_range[0], lower_limit_range[1] + steps, steps), 2).tolist()
